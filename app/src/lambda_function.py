bbd067cc39890143fc67f2d4ca0fe3c8514b2cd5
educastelo <dudu.sales08@hotmail.com>
2022-11-04 15:43:08 -0300

    first commit

--- app/src/lambda_function.py
+++ app/src/lambda_function.py
@@ -3,4 +3,4 @@ from aws_lambda_typing import context as context_
 
 def lambda_handler(event: dict, context: context_.Context):
     # Escreva o código da função aqui
-    print(event)
+    print("Ta funcionando 15:40")
